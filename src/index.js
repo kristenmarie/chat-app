5974427dc57e2386802c58a50518a58e411bcbee
Kristen Kulha <kristen.m.kulha@gmail.com>
2018-03-06 09:52:16 -0800

    Add provider and createStore imports/add future needed imports.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import chat from './reducers'
+
+const store = createStore(chat)
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
